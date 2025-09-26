2d74406f2ba95916d138a0ff6027b58da7bf0e20
Alishba Zakir Janjua <alishbazakirjanjua@gmail.com>
2025-09-26 16:11:32 +0500

    Add files via upload

---
+++ Lab 1 Alishba Zakir/Temprature.h
@@ -0,0 +1,16 @@
+//#include<iostream>
+//using namespace std;
+//class Temperature{
+//	public:
+//		float celsius;
+//		
+//		float toFahrenhiet(){
+//		return (celsius *9/5)+32;
+//		
+//}
+//void displayFahrenhiet(){
+//	float fahrenhiet = toFahrenhiet();
+//	cout <<"Temprature in fahrenhiet :"<<fahrenhiet<<endl;	
+//	}
+//	
+//};
